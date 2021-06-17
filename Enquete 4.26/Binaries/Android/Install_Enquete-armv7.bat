7e889a4d8c4fc2ed2ff50a3d45fd9e45e738cc0d
nealts <61749402+nealts@users.noreply.github.com>
2021-06-17 19:44:12 +0200

    oui

--- Enquete 4.26/Binaries/Android/Install_Enquete-armv7.bat
+++ Enquete 4.26/Binaries/Android/Install_Enquete-armv7.bat
@@ -1,7 +1,7 @@
 setlocal
 if NOT "%UE_SDKS_ROOT%"=="" (call %UE_SDKS_ROOT%\HostWin64\Android\SetupEnvironmentVars.bat)
 set ANDROIDHOME=%ANDROID_HOME%
-if "%ANDROIDHOME%"=="" set ANDROIDHOME=C:\Users\PRETE Florian\AppData\Local\Android\Sdk
+if "%ANDROIDHOME%"=="" set ANDROIDHOME=C:/Users/flori/AppData/Local/Android/Sdk
 set ADB=%ANDROIDHOME%\platform-tools\adb.exe
 set DEVICE=
 if not "%1"=="" set DEVICE=-s %1
