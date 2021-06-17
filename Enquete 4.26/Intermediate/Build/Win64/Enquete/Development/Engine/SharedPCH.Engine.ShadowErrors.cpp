cf2b47de9dabadb703199a6a570e59bcc8f2b5b9
nealts <61749402+nealts@users.noreply.github.com>
2021-06-17 18:50:16 +0200

    BON GROS GIT

--- Enquete 4.26/Intermediate/Build/Win64/Enquete/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Enquete 4.26/Intermediate/Build/Win64/Enquete/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.28.29337
-#include "C:/Users/PRETE Florian/Documents/GitHub/Enquete/Enquete 4.26/Intermediate/Build/Win64/Enquete/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "D:/Enquete/Enquete 4.26/Intermediate/Build/Win64/Enquete/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Enquete 4.26/Intermediate/Build/Win64/UE4/Development/Enquete/Definitions.Enquete.h
+++ Enquete 4.26/Intermediate/Build/Win64/UE4/Development/Enquete/Definitions.Enquete.h
@@ -34,14 +34,14 @@
 #define WITH_PUSH_MODEL 0
 #define WITH_CEF3 1
 #define WITH_LIVE_CODING 1
-#define UE_LIVE_CODING_ENGINE_DIR "C:\\Program Files\\Epic Games\\UE_4.26\\Engine"
-#define UE_LIVE_CODING_PROJECT "C:\\Users\\PRETE Florian\\Documents\\GitHub\\Enquete\\Enquete 4.26\\Enquete.uproject"
+#define UE_LIVE_CODING_ENGINE_DIR "D:\\UnrealGames\\UE_4.26\\Engine"
+#define UE_LIVE_CODING_PROJECT "D:\\Enquete\\Enquete 4.26\\Enquete.uproject"
 #define UBT_MODULE_MANIFEST "UE4.modules"
 #define UBT_MODULE_MANIFEST_DEBUGGAME "UE4-Win64-DebugGame.modules"
 #define UBT_COMPILED_PLATFORM Win64
 #define UBT_COMPILED_TARGET Game
 #define UE_APP_NAME "UE4"
-#define UE_ENGINE_DIRECTORY "../../../../../../../../Program Files/Epic Games/UE_4.26/Engine/"
+#define UE_ENGINE_DIRECTORY "../../../../UnrealGames/UE_4.26/Engine/"
 #define NDIS_MINIPORT_MAJOR_VERSION 0
 #define WIN32 1
 #define _WIN32_WINNT 0x0601
